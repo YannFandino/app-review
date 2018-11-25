1e3cfacee3de901ffd3fd311fbc199a1446c2ca9
Alba <adominguezpera@gmail.com>
2018-11-25 20:20:46 +0100

    Controlador y dao para review
    mas errata

---
+++ src/classes/controllers/ReviewController.php
@@ -0,0 +1,84 @@
+<?php
+namespace Classes\controllers;
+use Classes\daos\ReviewDao;
+
+class ReviewController {
+
+    public function add($req, $res, $args) {
+        $name = strtolower($args['product']);
+        $name = strtolower($args['user']);
+        $points = isset($args['points']) ? $args['points'] : null;
+        $comment = isset($args['comment']) ? $args['comment'] : null;
+        $date_created = isset($args['date_created']) ? $args['date_created'] : null;
+        $last_modified = isset($args['last_modified']) ? $args['last_modified'] : null;
+        $is_approved = isset($args['is_approved']) ? $args['is_approved'] : null;
+
+        $reviewDao = new ReviewDao();
+        $result = $reviewDao->addReview($product, $user, $points, $comment, $date_created, $last_modified, $is_approved);
+
+        if (!$result) {
+            echo $reviewDao->getError();
+        } else {
+            echo "Insertado";
+        }
+    }
+
+    public function update($req, $res, $args) {
+        $id = $args['id'];
+        $name = strtolower($args['product']);
+        $name = strtolower($args['user']);
+        $points = isset($args['points']) ? $args['points'] : null;
+        $comment = isset($args['comment']) ? $args['comment'] : null;
+        $date_created = isset($args['date_created']) ? $args['date_created'] : null;
+        $last_modified = isset($args['last_modified']) ? $args['last_modified'] : null;
+        $is_approved = isset($args['is_approved']) ? $args['is_approved'] : null;
+
+        $reviewDao = new ReviewDao();
+        $result = $reviewDao->updateReview($id, $product, $user, $points, $comment, $date_created, $last_modified, $is_approved);
+
+        if (!$result) {
+            echo $reviewDao->getError();
+        } else {
+            echo "Modificada";
+        }
+    }
+
+    public function listAll($req, $res, $args) {
+        $reviewDao = new ReviewDao();
+        $result = $reviewDao->getAll();
+
+        if (empty($result)) {
+            $msg = $reviewDao->getError() ? $reviewDao->getError() : "No hay valoraciones para mostrar";
+            echo $msg;
+        } else {
+            echo "<ul>";
+            foreach ($result as $arrayReview) {
+                echo "<strong>{$arrayReview->getId()}</strong>";
+                echo "<ul>";
+                echo "<li>{$arrayReview->getProduct()}</li>";
+                echo "<li>{$arrayReview->getUser()}</li>";
+                echo "<li>{$arrayReview->getPoints()}</li>";
+                echo "<li>{$arrayReview->getComment()}</li>";
+                echo "<li>{$arrayReview->getDateCreated()}</li>";
+                echo "<li>{$arrayReview->getLastModified()}</li>";
+                echo "<li>{$arrayReview->getisApproved()}</li>";
+                echo "</ul>";
+            }
+            echo "</ul>";
+        }
+    }
+
+    public function delete($req, $res, $args) {
+        $id = $args['id'];
+
+        $reviewDao = new ReviewDao();
+        $result = $reviewDao->deleteById($id);
+
+        if (!$result) {
+            echo $reviewDao->getError();
+        } else {
+            echo "Eliminada";
+        }
+    }
+
+}
---
+++ src/classes/daos/ReviewDao.php
@@ -0,0 +1,158 @@
+<?php
+namespace Classes\daos;
+
+use Classes\models\Review;
+use PDO;
+use DBConnection;
+use Exception;
+
+class ReviewDao {
+    private $db;
+    private $error;
+
+    /**
+     * ReviewDao constructor.
+     * @param $conn
+     */
+    public function __construct() {
+        $conn = new DBConnection();
+        $this->db = $conn->getConn();
+    }
+
+    public function getDb() {
+        return $this->db;
+    }
+
+    public function setError($error) {
+        $this->error = $error;
+    }
+
+    public function getError() {
+        return $this->error;
+    }
+
+    /** Añadir nuevo prodcuto
+     * @param $name
+     * @param $description
+     * @param $details
+     * @param $category
+     * @return boolean
+     */
+    public function addReview($product, $user, $points, $comment, $date_created, $last_modified, $is_approved) {
+        $db = $this->getDb();
+        $db->beginTransaction();
+        try {
+            if (!$this->isReviewExist($product, $user)) {
+                $sql = "INSERT INTO table_reviews (product_id, user_id, points, comment, date_created, last_modified, is_approved)
+                        VALUES (:product_id, :user_id, :points, :comment, :date_created, :last_modified, :is_approved)";
+                $stmt = $db->prepare($sql);
+                $stmt->bindParam('product_id', $product);
+                $stmt->bindParam('user_id', $user, PDO::PARAM_INT);
+                $stmt->bindParam('points', $points, PDO::PARAM_STR);
+                $stmt->bindParam('comment', $comment, PDO::PARAM_STR);
+                $stmt->bindParam('date_created', $date_created, PDO::PARAM_STR);
+                $stmt->bindParam('last_modified', $last_modified, PDO::PARAM_STR);
+                $stmt->bindParam('is_approved', $is_approved, PDO::PARAM_BOOL);
+                $result = $stmt->execute();
+
+                if ($result) {
+                    $db->commit();
+                    return true;
+                }
+            } else throw new Exception('Ya has valorado este producto');
+        } catch (Exception $e) {
+            $db->rollBack();
+            $this->setError($e->getMessage());
+            return false;
+        }
+
+    }
+
+    public function updateReview($id, $product, $user, $points, $comment, $date_created, $last_modified, $is_approved) {
+        $db = $this->getDb();
+        $db->beginTransaction();
+        try {
+            $sql = "UPDATE table_reviews
+                    SET product_id = :product_id,
+                    user_id = :user_id,
+                    comment = :comment,
+                    date_created = :date_created,
+                    last_modified = :last_modified,
+                    is_approved = :is_approved
+                    WHERE id = :id";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam('product_id', $product);
+            $stmt->bindParam('user_id', $user, PDO::PARAM_INT);
+            $stmt->bindParam('comment', $comment, PDO::PARAM_STR);
+            $stmt->bindParam('date_created', $date_created, PDO::PARAM_STR);
+            $stmt->bindParam('last_modified', $last_modified, PDO::PARAM_STR);
+            $stmt->bindParam('is_approved', $is_approved, PDO::PARAM_BOOL);
+            $result = $stmt->execute();
+
+            if ($result) {
+                $db->commit();
+                return true;
+            } else throw new Exception('Ha ocurrido un error al modificar la review');
+
+        } catch (Exception $e) {
+            $db->rollBack();
+            $this->setError($e->getMessage());
+            return false;
+        }
+    }
+
+    public function getAll() {
+        $db = $this->getDb();
+        $list = array();
+        $sql = "SELECT product_id, user_id, points, comment, date_created, last_modified, is_approved
+                FROM table_reviews 
+                ORDER BY name ASC";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+
+        while ($row = $stmt->fetch()) {
+            $review = new Review(null, $row['id'], $row['product_id'], $row['user_id'], $row['points'], $row['comment'], $row['date_created'], $row['last_modified'], $row['is_approved']);
+            $list[$review->getId()] = $review;
+            };
+        return $list;
+    }
+
+    public function deleteById($id) {
+        $db = $this->getDb();
+        $db->beginTransaction();
+        try {
+            $sql = "DELETE FROM table_reviews
+                    WHERE id = :id";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam('id', $id, PDO::PARAM_INT);
+            $result = $stmt->execute();
+
+            if ($result && $stmt->rowCount()) {
+                $db->commit();
+                return true;
+            } else throw new Exception('La review no existe.');
+        } catch (Exception $e) {
+            $db->rollBack();
+            $this->setError($e->getMessage());
+            return false;
+        }
+    }
+
+    public function isReviewExist($product, $user) {
+        $db = $this->getDb();
+        $sql = "SELECT * FROM table_reviews c
+                WHERE c.name = :name
+                AND c.user = :user";
+  
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam('name', $name);
+        $stmt->bindParam('user', $user);
+        $stmt->execute();
+        $row = $stmt->fetch();
+
+        if ($row) return true;
+        return false;
+    }
+
+
+}
